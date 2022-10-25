ffe26765d648420d61f2b3464bba27f8a59f7adf
HenryJamesGreen <henryjamesgreenmusic@gmail.com>
2022-10-25 23:49:50 +0100

    henrys update

--- henry.js
+++ henry.js
@@ -1,3 +1,8 @@
-function randomFunction() {
+function function1() {
+  alert("americaaaa fuck yeah");
   document.getElementById("rick").style.display = "block";
+}
+function function2() {
+  alert("wooohoooo");
+  document.getElementById("homer").style.display = "block"
 }
