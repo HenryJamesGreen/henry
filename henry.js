0f250007acfde524ecda8c786967676a14f6b0c5
HenryJamesGreen <henryjamesgreenmusic@gmail.com>
2022-10-28 01:11:37 +0100

    Add even more bits

--- henry.js
+++ henry.js
@@ -1,8 +1,4 @@
-function function1() {
-  alert("americaaaa fuck yeah");
-  document.getElementById("rick").style.display = "block";
-}
 function function2() {
   alert("wooohoooo");
-  document.getElementById("homer").style.display = "block"
+  document.getElementById("homer").style.display = "block";
 }
